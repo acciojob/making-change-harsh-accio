4d49bbbe2928b9ea6bdd92cdd36f0517427ccd11
harsh-accio <rajputharsh9897@gmail.com>
2025-09-17 17:15:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const makeChange = (c) => {
-  // your name here
+  // convert c to number
+  let amount = Number(c);
+
+  let q = Math.floor(amount / 25);
+  amount = amount % 25;
+
+  let d = Math.floor(amount / 10);
+  amount = amount % 10;
+
+  let n = Math.floor(amount / 5);
+  amount = amount % 5;
+
+  let p = amount;
+
+  return { q, d, n, p };
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
